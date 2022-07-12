eb772b4645063e96a250c0700f395931daa320e9
Thomas Rose <mail@tom-rose.de>
2022-07-12 10:46:59 +0000

    add urlify

--- challenges/urlify.js
+++ challenges/urlify.js
@@ -1,11 +1,12 @@
 function urlify(s) {
   // Write your code here
-  return s;
+  return encodeURI(s);
 }
 
 const { assert } = require('chai');
 describe("Tests", () => {
   it("tests", () => {
-    assert.equal(urlify('DE02200505501015871393'), true);
-  });
+    assert.equal(urlify('Hallo Welt'), 'Hallo%20Welt');
+    assert.equal(urlify('Vielen Dank, Thomas'), 'Vielen%20Dank,%20Thomas');
+    assert.equal(urlify('Schöne Bilder!'), 'Sch%C3%B6ne%20Bilder!');  });
 });

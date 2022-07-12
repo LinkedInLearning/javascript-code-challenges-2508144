6ed573518ed9757e1ca0002bcd23bdbde336afe4
Thomas Rose <mail@tom-rose.de>
2022-07-12 07:13:12 +0000

    add iban

--- challenges/iban.js
+++ challenges/iban.js
@@ -1,18 +1,19 @@
 function isIban(iban) {
-  // Write your code here
+  const rearranged = iban.substring(4, iban.length) + iban.substring(0, 4);
+  const numeric = Array.from(rearranged).map(c => (isNaN(parseInt(c)) ? (c.charCodeAt(0) - 64 + 9).toString() : c)).join('');
+  const remainder = Array.from(numeric).map(c => parseInt(c)).reduce((remainder, value) => (remainder * 10 + value) % 97, 0);
 
-  const bank = iban.substring(4,12);
-  const account = iban.substring(12, 22);
-  const country = (iban.substring(0, 2).charCodeAt(0) - 64 + 9) + "" + (iban.substring(0, 2).charCodeAt(1) - 64 + 9);
-
-  console.log(country)
-  return true;
+  return remainder === 1;
 }
 
 const { assert } = require('chai');
 describe("Tests", () => {
   it("tests", () => {
-    assert.equal(isIban('DE08700901001234567890'), true);
-   
+    assert.equal(isIban('DE02200505501015871393'), true);
+    assert.equal(isIban('DE88100900001234567892'), true);
+    assert.equal(isIban('AT026000000001349870'), true);
+    assert.equal(isIban('AT022081500000698597'), true);
+    assert.equal(isIban('CH0204835000626882001'), true);
+    assert.equal(isIban('CH0200790016271403331'), true);  
   });
 });

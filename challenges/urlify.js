4cc4a4f9dbd17cb33e4b84aa7449553db1c8a4e2
Thomas Rose <mail@tom-rose.de>
2022-07-12 09:00:41 +0000

    tidy isopen

---
+++ challenges/urlify.js
@@ -0,0 +1,11 @@
+function urlify(s) {
+  // Write your code here
+  return s;
+}
+
+const { assert } = require('chai');
+describe("Tests", () => {
+  it("tests", () => {
+    assert.equal(urlify('DE02200505501015871393'), true);
+  });
+});

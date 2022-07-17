d500dbdfa384b38c1c4652c6fddbeb5070f12d99
Thomas Rose <mail@tom-rose.de>
2022-07-17 11:06:51 +0000

    add test to 10

--- challenges/10allpossibilities.js
+++ challenges/10allpossibilities.js
@@ -10,5 +10,6 @@ describe("Tests", () => {
     assert.equal(isAllPossibilities([0, 1, 2, 3, 4, 5]), true);
     assert.equal(isAllPossibilities([1, 2, 3, 4]), false);
     assert.equal(isAllPossibilities([0, 1, 3, 4]), false);
+    assert.equal(isAllPossibilities([0, 1, 3, 2]), true);
   });
 });

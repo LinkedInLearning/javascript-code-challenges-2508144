1d9710c467fffeede2c2a0c7b18a982b7771be20
Thomas Rose <mail@tom-rose.de>
2022-07-17 12:04:58 +0000

    add weekdays to 12

--- challenges/12openorclosed.js
+++ challenges/12openorclosed.js
@@ -7,12 +7,12 @@ function isOpen(timestamp) {
 const { assert } = require('chai');
 describe("Tests", () => {
   it("tests", () => {
-    assert.equal(isOpen('2026-7-14 09:00'), true);
-    assert.equal(isOpen('2026-7-14 16:00'), true);
-    assert.equal(isOpen('2026-7-15 10:00'), true);
-    assert.equal(isOpen('2026-7-15 16:00'), false);
-    assert.equal(isOpen('2026-7-18 14:00'), false);
-    assert.equal(isOpen('2026-7-19 09:00'), false);
+    assert.equal(isOpen('2026-7-14 09:00'), true); // Tuesday
+    assert.equal(isOpen('2026-7-14 16:00'), true); // Tuesday
+    assert.equal(isOpen('2026-7-15 10:00'), true); // Wednesday
+    assert.equal(isOpen('2026-7-15 16:00'), false); // Wednesday
+    assert.equal(isOpen('2026-7-18 14:00'), false); // Saturday
+    assert.equal(isOpen('2026-7-19 09:00'), false); // Sunday
 
   });
 });
